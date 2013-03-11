78ee476e80e5593859a21d2d4c12171502e3d92e
张峥 <zz.zhangz@gmail.com>
2013-03-11 19:44:13 +0800

    Update t.py

--- t.py
+++ t.py
@@ -7,11 +7,13 @@ import app_singleton
 import time
 
 def main():
+	print "Start MyApp"
 	try:
 		app_singleton.app_singleton("/tmp/zz.pid")
 		time.sleep(100)
 	except Exception,msg:
 		print msg
+	print "End MyApp"
 
 if __name__ == "__main__":
         main()
